56cb31941347a70203437c91b6e1ef9f1ccc6bf3
fernandoslsz <slszfernando@gmail.com>
2020-05-08 21:26:18 -0300

    Using the facet to show datas in subplots.

--- second_file.R
+++ second_file.R
@@ -103,4 +103,6 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy, color = displ < 5))
 
 
-mpg
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_wrap(~ class, nrow = 2)
